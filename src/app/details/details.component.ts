b1fcb4381f5725956e790e59e6b632ccf5099e9d
LillaDou <lilla.e.j.du@gmail.com>
2024-08-14 15:17:27 +0200

    Enable and define routing

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,18 @@
+//This page provides more information on a given housing location.
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      details works!
+    </p>
+  `,
+  styles: ``
+})
+export class DetailsComponent {
+
+}
---
+++ src/app/routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from "@angular/router";
+import { DetailsComponent } from "./details/details.component";
+import { HomeComponent } from "./home/home.component";
+
+//Representa las rutas de la aplicación
+const routeConfig: Routes = [
+    //En este primero, cuando el url es igual al path, nos lleva al HomeComponent
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Home page',
+    },
+    {
+        path: 'details/:id',
+        component: DetailsComponent,
+        title: 'Home details',
+    },
+];
+export default routeConfig;
+
